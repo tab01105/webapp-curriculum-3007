0860318030e0ad90be700dd7972c78d86ccf2037
tab01105 <tab01105@gmail.com>
2018-02-14 14:01:51 +0900

    PromiseStdInの実装

--- src/main/scala/PromiseStdIn.scala
+++ src/main/scala/PromiseStdIn.scala
@@ -1,6 +1,6 @@
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
+import scala.concurrent.{Await, Future, Promise}
 import scala.util.{Failure, Success}
 
 object PromiseStdIn extends App {
@@ -9,7 +9,20 @@ object PromiseStdIn extends App {
     lineInputProcessor(io.StdIn.readLine().toInt)
   }
 
-  val future: Future[Int] = ???
+//  val future: Future[Int] = Future {
+  //    var y: Int = 0
+  //    applyFromStdIn({ x: Int => {
+  //      y = x * 7
+  //      Success(y)
+  //    }
+  //    })
+  //    y
+  //  }
+
+  val promise: Promise[Int] = Promise[Int]
+  applyFromStdIn((i) => promise.success(i * 7))
+
+  val future: Future[Int] = promise.future
 
   future onComplete {
     case Success(value) => println(value)
